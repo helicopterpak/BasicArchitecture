931779b19602a78f52b511a4923cb7cf17961f46
Pak <helicopterpak@gmail.com>
2023-06-14 13:27:21 +0300

    new task

--- app/src/main/java/ru/otus/basicarchitecture/MainActivity.kt
+++ app/src/main/java/ru/otus/basicarchitecture/MainActivity.kt
@@ -2,10 +2,17 @@ package ru.otus.basicarchitecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, PersonFragment.newInstance())
+            .commit()
+
     }
 }
